c09481a1bfe84f9800ac005cdfda4eb011354db5
siminino <victor.areas@corp.globo.com>
2017-04-13 15:45:13 -0300

    add color to buttons without background

--- website/src/components/pages/forms/Button.js
+++ website/src/components/pages/forms/Button.js
@@ -7,17 +7,20 @@ export default () => (
   <Page>
     <h1 className="bs-styleguide-page__title">Buttons</h1>
     <h2 className="bs-styleguide-page__subtitle">Usage</h2>
-    <Demo code={`<button class="bs-ui-button bs-ui-button--black">Button</button>
+    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black">Button</button>
 <button class="bs-ui-button">Button</button>`}>
       <p>There are two kinds of buttons: with background, to be used as primary actions and withoout background, fo secundary actions.</p>
     </Demo>
     <h2 className="bs-styleguide-page__subtitle">Colors</h2>
-    <Demo code={`<button class="bs-ui-button bs-ui-button--black">Button</button>
+    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black">Button</button>
+<button class="bs-ui-button bs-ui-button--background-blue">Button</button>
+<button class="bs-ui-button bs-ui-button--background-red">Button</button>
+<button class="bs-ui-button">Button</button>
 <button class="bs-ui-button bs-ui-button--blue">Button</button>
 <button class="bs-ui-button bs-ui-button--red">Button</button>`}/>
     <h2 className="bs-styleguide-page__subtitle">Size</h2>
-    <Demo code={`<button class="bs-ui-button bs-ui-button--black bs-ui-button--large">Button</button>
-<button class="bs-ui-button bs-ui-button--black">Button</button>
-<button class="bs-ui-button bs-ui-button--black bs-ui-button--small">Button</button>`}/>
+    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--large">Button</button>
+<button class="bs-ui-button bs-ui-button--background-black">Button</button>
+<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--small">Button</button>`}/>
   </Page>
 );

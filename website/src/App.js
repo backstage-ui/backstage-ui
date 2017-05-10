c2bb54869978ce8ee1d680afc0baf641e65c0d84
Victor de Oliveira Areas <victor.oliveira.ti@gmail.com>
2017-05-10 19:03:02 -0300

    Added Tabs Component (#5)

--- website/src/App.js
+++ website/src/App.js
@@ -21,6 +21,7 @@ import Controls from "./components/pages/forms/Controls";
 import Dropdown from "./components/pages/forms/Dropdown";
 import Breadcrumbs from "./components/pages/Breadcrumbs";
 import Table from "./components/pages/Table";
+import Tabs from "./components/pages/Tabs";
 
 import SideMenu from "./components/SideMenu";
 import Header from "./components/Header";
@@ -43,6 +44,7 @@ class App extends Component {
               <Route path="/button" component={Button}/>
               <Route path="/controls" component={Controls}/>
               <Route path="/dropdown" component={Dropdown}/>
+              <Route path="/tabs" component={Tabs}/>
               <Route path="/table" component={Table}/>
             </div>
           </div>
---
+++ website/src/components/pages/Tabs.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017, Globo.com (https://github.com/globocom)
+ *
+ * License: MIT
+ */
+
+import React from "react";
+
+import Demo from "../Demo";
+import Page from "../Page";
+
+
+export default () => (
+  <Page>
+    <h1 className="bs-styleguide-page__title">Tabs</h1>
+    <h2 className="bs-styleguide-page__subtitle">Usage</h2>
+    <p className="bs-styleguide-page__lead">
+    </p>
+    <Demo title="Regular size" code={`<div class="bs-ui-tabs">
+  <a href="#/tabs" class="bs-ui-tabs__tab bs-ui-tabs__tab--active">First Tab</a>
+  <a href="#/tabs" class="bs-ui-tabs__tab">Second Tab</a>
+  <a href="#/tabs" class="bs-ui-tabs__tab">Third Tab</a>
+</div>`}/>
+    <Demo title="Large size" code={`<div class="bs-ui-tabs bs-ui-tabs--large">
+  <a href="#/tabs" class="bs-ui-tabs__tab bs-ui-tabs__tab--active">First Tab</a>
+  <a href="#/tabs" class="bs-ui-tabs__tab">Second Tab</a>
+  <a href="#/tabs" class="bs-ui-tabs__tab">Third Tab</a>
+</div>`}/>
+    <Demo title="Small size" code={`<div class="bs-ui-tabs bs-ui-tabs--small">
+  <a href="#/tabs" class="bs-ui-tabs__tab bs-ui-tabs__tab--active">First Tab</a>
+  <a href="#/tabs" class="bs-ui-tabs__tab">Second Tab</a>
+  <a href="#/tabs" class="bs-ui-tabs__tab">Third Tab</a>
+</div>`}/>
+  </Page>
+);

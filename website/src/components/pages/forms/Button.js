1b67ed3291ddf113aac75f671434a7badfed2bb9
ntmedina <nataliamedina.rj@gmail.com>
2017-05-22 14:13:42 -0300

    Added button disabled style

--- website/src/components/pages/forms/Button.js
+++ website/src/components/pages/forms/Button.js
@@ -9,18 +9,21 @@ import React from "react";
 import Demo from "../../Demo";
 import Page from "../../Page";
 
+
 export default () => (
   <Page>
     <h1 className="bs-styleguide-page__title">Buttons</h1>
     <h2 className="bs-styleguide-page__subtitle">Types</h2>
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black"><span>Primary</span></button>
 <button class="bs-ui-button bs-ui-button--background-blue"><span>secondary</span></button>
-<button class="bs-ui-button bs-ui-button--background-red"><span>alert</span></button>`}>
+<button class="bs-ui-button bs-ui-button--background-red"><span>alert</span></button>
+<button class="bs-ui-button bs-ui-button--background-red bs-ui-button--disabled"><span>disabled</span></button>`}>
       <p>There are two kinds of buttons: with background and without backgrounds.</p>
     </Demo>
     <Demo code={`<button class="bs-ui-button"><span>primary</span></button>
 <button class="bs-ui-button bs-ui-button--blue"><span>secondary</span></button>
-<button class="bs-ui-button bs-ui-button--red"><span>alert</span></button>`}>
+<button class="bs-ui-button bs-ui-button--red"><span>alert</span></button>
+<button class="bs-ui-button bs-ui-button--red bs-ui-button--disabled"><span>disabled</span></button>`}>
     </Demo>
     <h2 className="bs-styleguide-page__subtitle">Size</h2>
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--large"><span>large</span></button>

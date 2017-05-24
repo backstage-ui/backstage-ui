ff83263238dbcc24dbcd48c92e49985eb14fcb2d
ntmedina <nataliamedina.rj@gmail.com>
2017-05-24 19:08:26 -0300

    Fixing lint

--- website/src/components/pages/forms/Button.js
+++ website/src/components/pages/forms/Button.js
@@ -18,16 +18,16 @@ export default () => (
       <p>There are two kinds of buttons: with background, to be used as primary actions and withoout background, to secundary actions.</p>
     </Demo>
     <h2 className="bs-styleguide-page__subtitle">Colors</h2>
-    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black">Button</button>
-<button class="bs-ui-button bs-ui-button--background-blue">Button</button>
-<button class="bs-ui-button bs-ui-button--background-red">Button</button>
-<button class="bs-ui-button">Button</button>
-<button class="bs-ui-button bs-ui-button--blue">Button</button>
-<button class="bs-ui-button bs-ui-button--red">Button</button>`}/>
+    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black"><span>Button</span></button>
+<button class="bs-ui-button bs-ui-button--background-blue"><span>Button</span></button>
+<button class="bs-ui-button bs-ui-button--background-red"><span>Button</span></button>
+<button class="bs-ui-button"><span>Button</span></button>
+<button class="bs-ui-button bs-ui-button--blue"><span>Button</span></button>
+<button class="bs-ui-button bs-ui-button--red"><span>Button</span></button>`}/>
     <h2 className="bs-styleguide-page__subtitle">Size</h2>
-    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--large">Button</button>
-<button class="bs-ui-button bs-ui-button--background-black">Button</button>
-<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--small">Button</button>`}/>
+    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--large"><span>Button</span></button>
+<button class="bs-ui-button bs-ui-button--background-black"><span>Button</span></button>
+<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--small"><span>Button</span></button>`}/>
     <h2 className="bs-styleguide-page__subtitle">Icons</h2>
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black"><span><i class="material-icons ic">cloud_upload</i>Button</span></button>
 <button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">cloud_upload</i></button>

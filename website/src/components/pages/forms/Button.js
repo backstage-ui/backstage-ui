916c48f311297f6bd1c75e30529d85dbdc943de2
Richard Kevin <richardkevin@gmail.com>
2017-05-26 10:11:44 -0300

    Create Optionlist
    Create Collapsed wrapper to buttons and change Dropdown to use optionlist
    
    Thanks to Natalia Medina @ntmedina

--- website/src/components/pages/forms/Button.js
+++ website/src/components/pages/forms/Button.js
@@ -14,24 +14,57 @@ export default () => (
   <Page>
     <h1 className="bs-styleguide-page__title">Buttons</h1>
     <h2 className="bs-styleguide-page__subtitle">Usage</h2>
+
+    <p className="bs-styleguide-page__lead">
+      There are two kinds of buttons: with background and without backgrounds.
+    </p>
+
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black">default</button>
 <button class="bs-ui-button bs-ui-button--background-blue">primary</button>
 <button class="bs-ui-button bs-ui-button--background-red">alert</button>
 <button class="bs-ui-button bs-ui-button--background-red bs-ui-button--disabled">disabled</button>`} title="Background">
-      <p>There are two kinds of buttons: with background and without backgrounds.</p>
     </Demo>
+
     <Demo code={`<button class="bs-ui-button">default</button>
 <button class="bs-ui-button bs-ui-button--blue">primary</button>
 <button class="bs-ui-button bs-ui-button--red">alert</button>
 <button class="bs-ui-button bs-ui-button--red bs-ui-button--disabled">disabled</button> `} title="Flat">
     </Demo>
+
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--large">large</button>
 <button class="bs-ui-button bs-ui-button--background-black">medium</button>
 <button class="bs-ui-button bs-ui-button--background-black bs-ui-button--small">small</button>`} title="Sizes"/>
+
     <h2 className="bs-styleguide-page__subtitle">Icons</h2>
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic ic--left">cloud_upload</i>upload</button>
 <button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">cloud_upload</i></button>`} title="Background"/>
+
     <Demo code={`<button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic ic--left">cloud_upload</i>upload</button>
 <button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic">cloud_upload</i></button>`} title="Flat"/>
+
+    <h2 className="bs-styleguide-page__subtitle">Group buttons</h2>
+
+    <h3 className="bs-styleguide-page__subtitle">Alignment</h3>
+    <Demo style={{height: "200px"}} code={`<div class="bs-ui-collapsed bs-ui-collapsed__button">
+  <button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">more_horiz</i></button>
+  <div class="bs-ui-options bs-ui-options--left bs-ui-options--open">
+    <div class="bs-ui-options__list">
+      <div class="bs-ui-options__item" key="haas-grotesk">Haas Grotesk</div>
+      <div class="bs-ui-options__item bs-ui-options__item--selected"  key="helvetica">Helvetica</div>
+      <div class="bs-ui-options__item" key="proxima-nova">Proxima Nova</div>
+    </div>
+  </div>
+</div>`} title="Left" />
+
+        <Demo style={{height: "200px"}} code={`<div class="bs-ui-collapsed bs-ui-collapsed__button" style="float: right">
+    <button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">more_horiz</i></button>
+  <div class="bs-ui-options bs-ui-options--right bs-ui-options--open">
+    <div class="bs-ui-options__list">
+      <div class="bs-ui-options__item" key="haas-grotesk">Haas Grotesk</div>
+      <div class="bs-ui-options__item bs-ui-options__item--selected"  key="helvetica">Helvetica</div>
+      <div class="bs-ui-options__item" key="proxima-nova">Proxima Nova</div>
+    </div>
+  </div>
+</div>`} title="Right" />
   </Page>
 );
---
+++ website/src/components/pages/forms/Options.js
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) 2017, Globo.com (https://github.com/globocom)
+ *
+ * License: MIT
+ */
+
+
+import React from "react";
+
+import Demo from "../../Demo";
+import Page from "../../Page";
+
+
+export default () => {
+  const openedStyle = {
+    height: "160px"
+  };
+  const maxHeight = {
+    height: "220px"
+  };
+
+  return (
+    <Page>
+      <h1 className="bs-styleguide-page__title">Options</h1>
+      <p className="bs-styleguide-page__lead">
+        The Options component is a cardified list of itens. Its commonly used in
+        conjunction with other components like buttons, inputs and dropdowns.
+      </p>
+
+      <h2 className="bs-styleguide-page__subtitle">Default</h2>
+      <Demo style={openedStyle} code={`<div style="width: 100%; position: relative; ">
+  <div class="bs-ui-options bs-ui-options--open">
+    <div class="bs-ui-options__list">
+      <div class="bs-ui-options__item" key="haas-grotesk">Haas Grotesk</div>
+      <div class="bs-ui-options__item bs-ui-options__item--selected"  key="helvetica">Helvetica</div>
+      <div class="bs-ui-options__item" key="proxima-nova">Proxima Nova</div>
+    </div>
+  </div>
+</div>
+`} title="Default" />
+
+      <Demo style={openedStyle} code={`<div style="width: 100%; position: relative; ">
+  <div class="bs-ui-options bs-ui-options--open">
+    <div class="bs-ui-options__list">
+      <div class="bs-ui-options__item" key="haas-grotesk">Haas Grotesk</div>
+      <div class="bs-ui-options__item"  key="helvetica">Helvetica</div>
+      <div class="bs-ui-options__item bs-ui-options__item--divider" key="proxima-nova">Proxima Nova</div>
+    </div>
+  </div>
+</div>
+`} title="Divider" />
+
+    <Demo style={maxHeight} code={`<div style="width: 100%; position: relative; ">
+  <div class="bs-ui-options bs-ui-options--open">
+    <div class="bs-ui-options__header">
+      <div class="bs-ui-options__title">Select Type</div>
+      <div class="bs-ui-options__subtitle">You can change anytime!</div>
+    </div>
+    <div class="bs-ui-options__list">
+      <div class="bs-ui-options__item" key="haas-grotesk">Haas Grotesk</div>
+      <div class="bs-ui-options__item bs-ui-options__item--selected"  key="helvetica">Helvetica</div>
+      <div class="bs-ui-options__item" key="proxima-nova">Proxima Nova</div>
+    </div>
+  </div>
+</div>
+`} title="Title & Subtitle" />
+
+      <Demo style={maxHeight} code={`<div style="width: 100%; position: relative; ">
+  <div class="bs-ui-options bs-ui-options--small bs-ui-options--open">
+    <div class="bs-ui-options__header">
+      <div class="bs-ui-options__title">Select Type</div>
+      <div class="bs-ui-options__subtitle">You can change anytime!</div>
+    </div>
+    <div class="bs-ui-options__list">
+      <div class="bs-ui-options__item" key="haas-grotesk">Haas Grotesk</div>
+      <div class="bs-ui-options__item bs-ui-options__item--selected"  key="helvetica">Helvetica</div>
+      <div class="bs-ui-options__item" key="proxima-nova">Proxima Nova</div>
+    </div>
+  </div>
+</div>
+`} title="Small" />
+
+      <h2 className="bs-styleguide-page__subtitle">Avatar</h2>
+      <Demo style={{height: "200px"}} code={`<div style="width: 100%; position: relative; ">
+  <div class="bs-ui-options bs-ui-options--open">
+    <div class="bs-ui-options__list">
+      <li class="bs-ui-options__item">
+        <img class="bs-ui-options__avatar" src="/bs-logo.png">
+        Backstage 1
+      </li>
+      <li class="bs-ui-options__item">
+        <img class="bs-ui-options__avatar" src="/bs-logo.png">
+          Backstage 2
+      </li>
+      <li class="bs-ui-options__item">
+        <img class="bs-ui-options__avatar" src="/bs-logo.png">
+          Backstage 3
+      </li>
+    </div>
+  </div>
+</div>
+`} title="Default" />
+
+      <Demo style={openedStyle} code={`<div style="width: 100%; position: relative; ">
+  <div class="bs-ui-options bs-ui-options--small bs-ui-options--open">
+    <div class="bs-ui-options__list">
+      <li class="bs-ui-options__item">
+        <img class="bs-ui-options__avatar" src="/bs-logo.png">
+        Backstage 1
+      </li>
+      <li class="bs-ui-options__item">
+        <img class="bs-ui-options__avatar" src="/bs-logo.png">
+          Backstage 2
+      </li>
+      <li class="bs-ui-options__item">
+        <img class="bs-ui-options__avatar" src="/bs-logo.png">
+          Backstage 3
+      </li>
+    </div>
+  </div>
+</div>
+`} title="Small" />
+
+      <h2 className="bs-styleguide-page__subtitle">Icon</h2>
+      <Demo style={openedStyle} code={`<div style="width: 100%; position: relative; ">
+  <div class="bs-ui-options bs-ui-options--open">
+    <div class="bs-ui-options__list">
+      <li class="bs-ui-options__item">
+        <a><i class="material-icons ic bs-ui-options__icon">delete</i>Delete</a>
+      </li>
+      <li class="bs-ui-options__item">
+        <a><i class="material-icons ic bs-ui-options__icon">mode_edit</i>Edit</a>
+      </li>
+      <li class="bs-ui-options__item">
+        <a><i class="material-icons ic bs-ui-options__icon">add_box</i>Add</a>
+      </li>
+    </div>
+  </div>
+</div>
+`} title="Default" />
+
+      <Demo style={openedStyle} code={`<div style="width: 100%; position: relative; ">
+  <div class="bs-ui-options bs-ui-options--small bs-ui-options--open">
+    <div class="bs-ui-options__list">
+      <li class="bs-ui-options__item">
+        <a><i class="material-icons ic bs-ui-options__icon">delete</i>Delete</a>
+      </li>
+      <li class="bs-ui-options__item">
+        <a><i class="material-icons ic bs-ui-options__icon">mode_edit</i>Edit</a>
+      </li>
+      <li class="bs-ui-options__item">
+        <a><i class="material-icons ic bs-ui-options__icon">add_box</i>Add</a>
+      </li>
+    </div>
+  </div>
+</div>
+`} title="Small" />
+
+    </Page>
+  );
+};

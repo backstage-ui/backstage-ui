ba133aca73e1ca8837c88f7101c2f95073fd21ba
ntmedina <nataliamedina.rj@gmail.com>
2017-06-06 18:28:42 -0300

    Adjusting buttons sctructure

--- website/src/components/pages/forms/Button.js
+++ website/src/components/pages/forms/Button.js
@@ -13,28 +13,25 @@ import Page from "../../Page";
 export default () => (
   <Page>
     <h1 className="bs-styleguide-page__title">Buttons</h1>
-    <h2 className="bs-styleguide-page__subtitle">Types</h2>
-    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black"><span>Primary</span></button>
-<button class="bs-ui-button bs-ui-button--background-blue"><span>secondary</span></button>
-<button class="bs-ui-button bs-ui-button--background-red"><span>alert</span></button>
-<button class="bs-ui-button bs-ui-button--background-red bs-ui-button--disabled"><span>disabled</span></button>`}>
+    <h2 className="bs-styleguide-page__subtitle">Usage</h2>
+    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black">default</button>
+<button class="bs-ui-button bs-ui-button--background-blue">primary</button>
+<button class="bs-ui-button bs-ui-button--background-red">alert</button>
+<button class="bs-ui-button bs-ui-button--background-red bs-ui-button--disabled">disabled</button>`} title="Background">
       <p>There are two kinds of buttons: with background and without backgrounds.</p>
     </Demo>
-    <Demo code={`<button class="bs-ui-button"><span>primary</span></button>
-<button class="bs-ui-button bs-ui-button--blue"><span>secondary</span></button>
-<button class="bs-ui-button bs-ui-button--red"><span>alert</span></button>
-<button class="bs-ui-button bs-ui-button--red bs-ui-button--disabled"><span>disabled</span></button>`}>
+    <Demo code={`<button class="bs-ui-button">default</button>
+<button class="bs-ui-button bs-ui-button--blue">primary</button>
+<button class="bs-ui-button bs-ui-button--red">alert</button>
+<button class="bs-ui-button bs-ui-button--red bs-ui-button--disabled">disabled</button> `} title="Flat">
     </Demo>
-    <h2 className="bs-styleguide-page__subtitle">Size</h2>
-    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--large"><span>large</span></button>
-<button class="bs-ui-button bs-ui-button--background-black"><span>medium</span></button>
-<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--small"><span>small</span></button>`}/>
+    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--large">large</button>
+<button class="bs-ui-button bs-ui-button--background-black">medium</button>
+<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--small">small</button>`} title="Sizes"/>
     <h2 className="bs-styleguide-page__subtitle">Icons</h2>
-    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black"><span><i class="material-icons ic">cloud_upload</i>upload</span></button>
-<button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">cloud_upload</i></button>
-<button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">more_horiz</i></button>`}/>
-    <Demo code={`<button class="bs-ui-button bs-ui-button--black"><span><i class="material-icons ic">cloud_upload</i>upload</span></button>
-<button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic">cloud_upload</i></button>
-<button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic">more_horiz</i></button>`}/>
+    <Demo code={`<button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic ic--left">cloud_upload</i>upload</button>
+<button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">cloud_upload</i></button>`} title="Background"/>
+    <Demo code={`<button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic ic--left">cloud_upload</i>upload</button>
+<button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic">cloud_upload</i></button>`} title="Flat"/>
   </Page>
 );

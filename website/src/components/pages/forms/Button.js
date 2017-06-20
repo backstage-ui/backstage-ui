0ebce5ef95aeb1e1338b94118739bd1263b7970d
ntmedina <nataliamedina.rj@gmail.com>
2017-06-20 10:59:34 -0300

    Updating info

--- website/src/components/pages/forms/Button.js
+++ website/src/components/pages/forms/Button.js
@@ -22,13 +22,13 @@ export default () => (
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black">default</button>
 <button class="bs-ui-button bs-ui-button--background-blue">primary</button>
 <button class="bs-ui-button bs-ui-button--background-red">alert</button>
-<button class="bs-ui-button bs-ui-button--background-red bs-ui-button--disabled">disabled</button>`} title="Background">
+<button class="bs-ui-button bs-ui-button--background-red bs-ui-button--disabled">disabled</button>`} >
     </Demo>
 
     <Demo code={`<button class="bs-ui-button">default</button>
 <button class="bs-ui-button bs-ui-button--blue">primary</button>
 <button class="bs-ui-button bs-ui-button--red">alert</button>
-<button class="bs-ui-button bs-ui-button--red bs-ui-button--disabled">disabled</button> `} title="Flat">
+<button class="bs-ui-button bs-ui-button--red bs-ui-button--disabled">disabled</button> `} >
     </Demo>
 
     <p className="bs-styleguide-page__lead">
@@ -37,17 +37,17 @@ export default () => (
 
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--large">large</button>
 <button class="bs-ui-button bs-ui-button--background-black">medium</button>
-<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--small">small</button>`} title="Sizes"/>
+<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--small">small</button>`} />
 
     <h2 className="bs-styleguide-page__subtitle">Icons and Labels</h2>
     <p className="bs-styleguide-page__lead">
       You can use a button with or without an icon. In case of a button with icon and label, it must pass a message in the most clear and objective way.
     </p>
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic ic--left">cloud_upload</i>upload</button>
-<button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">cloud_upload</i></button>`} title="Background"/>
+<button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">cloud_upload</i></button>`} />
 
     <Demo code={`<button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic ic--left">cloud_upload</i>upload</button>
-<button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic">cloud_upload</i></button>`} title="Flat"/>
+<button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic">cloud_upload</i></button>`} />
 
     <h2 className="bs-styleguide-page__subtitle">Collapsed buttons</h2>
     <p className="bs-styleguide-page__lead">
@@ -62,9 +62,8 @@ export default () => (
       <div class="bs-ui-options__item" key="proxima-nova">Proxima Nova</div>
     </div>
   </div>
-</div>`} title="Left" />
-
-        <Demo style={{height: "200px"}} code={`<div class="bs-ui-collapsed bs-ui-collapsed__button" style="float: right">
+</div>
+<div class="bs-ui-collapsed bs-ui-collapsed__button" style="float: right">
     <button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">more_horiz</i></button>
   <div class="bs-ui-options bs-ui-options--right bs-ui-options--open">
     <div class="bs-ui-options__list">
@@ -73,6 +72,6 @@ export default () => (
       <div class="bs-ui-options__item" key="proxima-nova">Proxima Nova</div>
     </div>
   </div>
-</div>`} title="Right" />
+</div>`} />
   </Page>
 );

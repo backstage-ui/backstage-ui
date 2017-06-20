6d22c6cef63d1a131e9748ddc922d3138b3661a5
ntmedina <nataliamedina.rj@gmail.com>
2017-06-20 10:54:53 -0300

    Updating buttons section info

--- website/src/components/pages/forms/Button.js
+++ website/src/components/pages/forms/Button.js
@@ -16,7 +16,7 @@ export default () => (
     <h2 className="bs-styleguide-page__subtitle">Usage</h2>
 
     <p className="bs-styleguide-page__lead">
-      There are two kinds of buttons: with background and without backgrounds.
+      There are two kinds of buttons: with background and without background.
     </p>
 
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black">default</button>
@@ -30,6 +30,10 @@ export default () => (
 <button class="bs-ui-button bs-ui-button--red">alert</button>
 <button class="bs-ui-button bs-ui-button--red bs-ui-button--disabled">disabled</button> `} title="Flat">
     </Demo>
+
+    <p className="bs-styleguide-page__lead">
+      You must choose one of the three different sizes available: large, medium or small.
+    </p>
 
     <Demo code={`<button class="bs-ui-button bs-ui-button--background-black bs-ui-button--large">large</button>
 <button class="bs-ui-button bs-ui-button--background-black">medium</button>
@@ -46,8 +50,9 @@ export default () => (
 <button class="bs-ui-button bs-ui-button--black"><i class="material-icons ic">cloud_upload</i></button>`} title="Flat"/>
 
     <h2 className="bs-styleguide-page__subtitle">Collapsed buttons</h2>
-
-    <h3 className="bs-styleguide-page__subtitle">Alignment</h3>
+    <p className="bs-styleguide-page__lead">
+      You can align a collapsed button in the left or in the right side of page.
+    </p>
     <Demo style={{height: "200px"}} code={`<div class="bs-ui-collapsed bs-ui-collapsed__button">
   <button class="bs-ui-button bs-ui-button--background-black"><i class="material-icons ic">more_horiz</i></button>
   <div class="bs-ui-options bs-ui-options--left bs-ui-options--open">
